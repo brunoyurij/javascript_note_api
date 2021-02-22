9bdf40cf8b702ca8a0040acb64e4d14d308a61cd
Bruno Yuri Jacinto <bruno.orle@gmail.com>
2021-02-22 08:26:09 -0300

    Crated update in user, password and remove account

--- app/routes/users.js
+++ app/routes/users.js
@@ -2,6 +2,7 @@ import express from 'express'
 import jwt from 'jsonwebtoken'
 import dotenv from 'dotenv'
 import User from '../models/user.js'
+import withAuth from '../middlewares/auth'
 
 dotenv.config()
 
@@ -44,6 +45,49 @@ router.post('/login', async (req, res) => {
         }
     } catch (error) {
         res.status(500).json({ error: 'Internal error, please try again' })
+    }
+})
+
+router.put('/', withAuth, async (req, res) => {
+    const { name, email } = req.body
+
+    try {
+        const user = await User.findOneAndUpdate(
+            { _id: req.user.id },
+            { $set: { name, email } },
+            { upsert: true, new: true }
+        )
+
+        res.status(200).json(user)
+    } catch (err) {
+        res.status(500).json(err)
+    }
+})
+
+router.put('/password', withAuth, async (req, res) => {
+    const { password } = req.body
+
+    try {
+        const user = await User.findOne({ _id: req.user.id })
+
+        user.password = password
+        await user.save()
+
+        res.status(200).json(user)
+    } catch (err) {
+        res.status(500).json(err)
+    }
+})
+
+router.delete('/', withAuth, async (req, res) => {
+    try {
+        const user = await User.findOne({ _id: req.user.id })
+
+        await user.delete()
+
+        res.json({ message: 'OK' }).status(201)
+    } catch (err) {
+        res.status(500).json(err)
     }
 })
 
