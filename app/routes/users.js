24cd981f6084ff5173f77dd822ff281e415d17c6
Bruno Yuri Jacinto <bruno.orle@gmail.com>
2021-02-22 09:05:50 -0300

    Update routes

--- app/routes/users.js
+++ app/routes/users.js
@@ -2,7 +2,7 @@ import express from 'express'
 import jwt from 'jsonwebtoken'
 import dotenv from 'dotenv'
 import User from '../models/user.js'
-import withAuth from '../middlewares/auth'
+import withAuth from '../middlewares/auth.js'
 
 dotenv.config()
 
