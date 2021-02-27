aba8b27cf9ed82cb7a58181558f4ad82e30e772d
Bruno Yuri Jacinto <bruno.orle@gmail.com>
2021-02-27 01:11:51 -0300

    Alterado para utilizar dinamicamente a url do mongo db

--- config/database.js
+++ config/database.js
@@ -1,9 +1,12 @@
 import mongoose from 'mongoose'
+import dotenv from 'dotenv'
+
+dotenv.config()
 
 mongoose.Promise = global.Promise
 
 mongoose
-    .connect('mongodb://localhost/javascriptNote', {
+    .connect(process.env.MONGO_URL, {
         useNewUrlParser: true,
         useUnifiedTopology: true,
         useCreateIndex: true,
