10d50cb3335e26f94657cb6853352e8510e3d176
Bruno Yuri Jacinto <bruno.orle@gmail.com>
2021-02-27 11:58:42 -0300

    Ajustado cors

--- app.js
+++ app.js
@@ -18,17 +18,7 @@ app.use(logger('dev'))
 
 app.use(express.json())
 
-// app.use(cors())
-app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', '*')
-    res.header('Access-Control-Allow-Credentials', true)
-    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS')
-    res.header(
-        'Access-Control-Allow-Headers',
-        'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json'
-    )
-    next()
-})
+app.use(cors())
 
 app.use(express.urlencoded({ extended: false }))
 
